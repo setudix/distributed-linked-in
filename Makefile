888f858af7e008c87f84d6668f2114681db9fa5f
setu <setu@setu.com>
2023-11-06 05:03:40 +0600

    userdb configuration

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 build:
 	docker compose build 
 	cd user-service && docker compose build 
+	cd user-service && cd userdb && docker compose build 
 
 up:
 	docker compose up -d
 	cd user-service && docker compose up -d
-
+	cd user-service && cd userdb && docker compose up -d 
 down: 
 	docker compose down 
 	cd user-service && docker compose down 
+	cd user-service && cd userdb && docker compose down
+
+user-service-shell:
+	cd user-service && docker compose exec -it user-service-php bash
+	
--- user-service/src/app/Models/User.php
+++ user-service/src/app/Models/User.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use MongoDB\Laravel\Eloquent\Model;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    protected $connection = 'mongodb';
+    protected $collection = 'users';
+
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
